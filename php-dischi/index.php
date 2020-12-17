6672f5f224e1da8ce00ccc49b9f0511b2f7256af
Daoud <99m.daoud@gmail.com>
2020-12-17 15:51:23 +0100

    GENERALE:  aggiunto stile alla pagina; PHP: aggiunto header

--- php-dischi/index.php
+++ php-dischi/index.php
@@ -4,17 +4,33 @@
     <head>
         <meta charset="utf-8">
         <title>php dischi</title>
+        <link rel="stylesheet" href="./../public/app.css">
     </head>
     <body>
-        <?php
-            foreach ($dischi as $disco) { ?>
-                <div class="card">
-                    <img src="<?php echo $disco['poster']; ?>" alt="<?php echo $disco['title']; ?>">
-                    <h5 class="song-title"><?php echo $disco['title']; ?></h5>
-                    <div class="song-author"><?php echo $disco['author']; ?></div>
-                    <small><?php echo $disco['year']; ?></small>
-                </div> <?php
-            }
-        ?>
+        <header>
+            <div class="container">
+                <div class="logo-container">
+                    <img src="https://www.pngkey.com/png/full/48-481733_spotify-logo-button-vector-spotify-white-logo-transparent.png" alt="spotify logo">
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <div class="card-container">
+                    <?php
+                    foreach ($dischi as $disco) { ?>
+                        <div class="card">
+                            <div class="poster-container">
+                                <img src="<?php echo $disco['poster']; ?>" alt="<?php echo $disco['title']; ?>">
+                            </div>
+                            <h5 class="song-title"><?php echo $disco['title']; ?></h5>
+                            <div class="song-author"><?php echo $disco['author']; ?></div>
+                            <small class="song-year"><?php echo $disco['year']; ?></small>
+                        </div> <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </main>
     </body>
 </html>
